5565538afbe5677535b5ac9e00250e7194a03a3f
hamtos <135288715+hamtos@users.noreply.github.com>
2023-06-06 01:44:56 +0000

    [finish]lesson6-2

---
+++ lesson6-2.rb
@@ -0,0 +1,9 @@
+puts "二つの数字を入力してください！"
+input_num1 = gets.to_i
+input_num2 = gets.to_i
+
+puts "計算結果を表示します。"
+puts "#{input_num1}+#{input_num2}=#{input_num1 + input_num2}"
+puts "#{input_num1}-#{input_num2}=#{input_num1 - input_num2}"
+puts "#{input_num1}*#{input_num2}=#{input_num1 * input_num2}"
+puts "#{input_num1}/#{input_num2}=#{input_num1 / input_num2}"
